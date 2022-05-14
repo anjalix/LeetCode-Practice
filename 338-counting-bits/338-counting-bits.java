09ad998ced98fb33ad01de8d50ff21747cf20ff2
Anjali Kumari <anjalinits1@gmail.com>
2022-05-15 01:36:32 +0530

    Time: 3 ms (44.25%), Space: 48.5 MB (24.15%) - LeetHub

--- 338-counting-bits/338-counting-bits.java
+++ 338-counting-bits/338-counting-bits.java
@@ -1,9 +1,14 @@
-class Solution {
-    public int[] countBits(int n) {
-        int[] ans = new int[n+1];
-        for(int i =0;i<=n; i++)
-            ans[i]= Integer.bitCount(i);
-        return ans;
+class Solution {    
+    public int count(int n) {
+        if(n<=0) 
+            return 0;
+        return ((n&1) + count(n>>>1));
     }
     
+    public int[] countBits(int n) {
+        int[] ans = new int[n+1];        
+        for(int i = 0 ;i<=n; i++)
+            ans[i] = count(i);       
+        return ans;
+    }    
 }
