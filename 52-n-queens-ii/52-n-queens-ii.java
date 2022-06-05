6d49b6ac922bae8b8cd24e82ec431f4d864db572
Anjali Kumari <anjalinits1@gmail.com>
2022-06-05 13:32:59 +0530

    Time: 2 ms (84.08%), Space: 39 MB (92.30%) - LeetHub

---
+++ 52-n-queens-ii/52-n-queens-ii.java
@@ -0,0 +1,23 @@
+public class Solution {
+    int count=0;
+    public int totalNQueens(int n) {
+        dfs(new int[n],0,n);
+        return count;
+    }
+    public void dfs(int[] pos,int step,int n) {
+        if(step==n) {
+            count++;
+            return;
+        }
+        for(int i=0;i<n;i++) {
+            pos[step]=i;
+            if(isvalid(pos,step)) dfs(pos,step+1,n);
+        }
+    }
+    public boolean isvalid(int[] pos, int step) {
+        for(int i=0;i<step;i++) {
+            if(pos[i]==pos[step]||(Math.abs(pos[i]-pos[step]))==(step-i)) return false;
+        }
+        return true;
+    } 
+}
