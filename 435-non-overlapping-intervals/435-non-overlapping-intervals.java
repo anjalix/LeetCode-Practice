5d69e20723189d7ceec88c74318ee927ef8a7529
Anjali Kumari <anjalinits1@gmail.com>
2022-06-09 20:35:13 +0530

    Time: 97 ms (32.08%), Space: 101.7 MB (28.11%) - LeetHub

--- 435-non-overlapping-intervals/435-non-overlapping-intervals.java
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.java
@@ -1,3 +1,42 @@
+/*
+Solution 2
+Sorting on the basis of starting
+*/
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        Arrays.sort(intervals, (a,b) -> (a[0]-b[0]));
+        int n = intervals.length;
+        int cnt = 1;
+        int curr = 0;
+        int next = 1;
+        
+        while(next < n) {
+            //Case 1: Non Overlapping
+            // End of curr is less or equal to start of next
+            if(intervals[curr][1] <= intervals[next][0]) {
+                curr = next;
+                cnt++;
+                next++;
+            }
+            //Case 2: Overlapping Case
+            // End of curr is more than start of next
+            else if(intervals[curr][1] > intervals[next][0]) {
+                if(intervals[next][1] < intervals[curr][1])
+                curr = next;
+                next++;
+            }                        
+        }
+        return n-cnt;
+        
+    }
+}
+
+
+
+/*
+Solution 1
+Sorting on the basis of ending
+
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
         Arrays.sort(intervals, new Comparator<int[]>(){
@@ -17,3 +56,5 @@ class Solution {
         return intervals.length-cnt;
     }
 }
+
+*/
