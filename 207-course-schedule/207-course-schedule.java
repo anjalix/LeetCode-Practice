6293c35cea05b8dd3fc811abde4954ee31cc2982
Anjali Kumari <anjalinits1@gmail.com>
2022-06-10 21:16:43 +0530

    Time: 5 ms (81.05%), Space: 48.1 MB (47.81%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.java
@@ -0,0 +1,55 @@
+class Solution {
+    
+    Graph graph;
+    
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        graph = new Graph(numCourses);
+        for(int[] p : prerequisites)
+            graph.addLink(p[1] , p[0]);
+        return !isCyclic();
+    }
+    
+    public boolean isCyclic() {
+        boolean[] visited = new boolean[graph.v];
+        boolean[] recStack = new boolean[graph.v];
+        
+        for(int i = 0;i<graph.v;i++)
+            if(isCyclicUtil(i, visited, recStack))
+                return true;
+        return false;        
+    }
+    
+    public boolean isCyclicUtil(int i, boolean[] visited, boolean[] recStack) {
+        if(recStack[i])
+            return true;
+        if(visited[i])
+            return false;
+        recStack[i] = true;
+        visited[i] = true;
+        List<Integer> children = graph.adj.get(i);
+        
+        for(int child : children) {
+            if(isCyclicUtil(child, visited, recStack))
+                return true;
+        }
+        recStack[i] = false;
+        return false;
+    }
+    
+}
+
+class Graph{
+    public static int v;
+    public static List<List<Integer>> adj;
+    
+    public Graph(int v) {
+        this.v = v;
+        adj = new ArrayList<List<Integer>>(v);
+        for(int i = 0;i<v;i++)
+            adj.add(new LinkedList<>());
+    }
+    
+    public static void addLink(int src, int dest) {
+        adj.get(src).add(dest);
+    }
+}
