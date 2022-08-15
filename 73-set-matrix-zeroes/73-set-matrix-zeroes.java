90bc246a8e11b94dd63440ba2443a866eec350f5
Anjali Kumari <anjalinits1@gmail.com>
2022-08-15 13:56:11 +0530

    Time: 1 ms (97.40%), Space: 54.3 MB (38.73%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -1,3 +1,64 @@
+ class Solution {
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int j_flag = 1;
+        int i_flag = 1;
+
+        for(int i = 0; i<m;i++) {
+            for(int j = 0; j<n; j++){
+                if(matrix[i][j] == 0) {
+                    if(i!=0 && j!=0) {
+                        matrix[0][j] = 0;
+                        matrix[i][0] = 0;
+                    }
+                    if(i == 0 && j ==0) {
+                        i_flag = 0;
+                        j_flag = 0;
+                    }
+                    if(j == 0) {
+                        j_flag = 0;
+                    }
+                    else if(i == 0) {
+                        i_flag = 0;
+                    }
+                }
+            }
+        }
+        // Mark it as 0
+        for(int i = 1; i<m;i++) {
+            if(matrix[i][0] == 0) {
+                for(int j = 0; j<n; j++){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // Mark it as 0
+        for(int j = 1; j<n;j++) {
+            if(matrix[0][j] == 0) {
+                for(int i = 0; i<m; i++){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        if(i_flag == 0) {
+            for(int j = 0; j<n; j++){
+                    matrix[0][j] = 0;
+            }
+        }
+
+        if(j_flag == 0) {
+            for(int i = 0; i<m; i++){
+                matrix[i][0] = 0;
+            }
+        }
+    }
+}
+
+
+/*
 class Solution {
     public void setZeroes(int[][] matrix) {
         List<Integer> row = new ArrayList<Integer>();        
@@ -28,3 +89,4 @@ class Solution {
         }
     }
 }
+*/
